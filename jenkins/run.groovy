8ca97e7ccd9b6ac8f67b51da3afd12596f1a3566
radu <radu@crownstudio.ro>
2023-06-02 23:56:19 +0300

    Added run Jenkinsfile

---
+++ jenkins/run.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    parameters {
+        string(name: "DOCKER_TAG", defaultValue: "latest", description: "The tag of the Docker Image to pull")
+        string(name: "SUITE", defaultValue: "suite.xml", description: "The file name of the suite to run.")
+        string(name: "QUEUE", defaultValue: "q.request.test", description: "Change this if you need to run on a different queue")
+    }
+    stages {
+        stage("Run Suite") {
+            steps {
+                script {
+                    sh "docker pull radusimonica/test_distributor:$DOCKER_TAG"
+                    withCredentials([usernamePassword(credentialsId: 'rabbit-credentials', passwordVariable: 'RABBIT_PASS', usernameVariable: 'RABBIT_USER')]) {
+                        sh "docker run -e RABBITMQ_DEFAULT_USER=$RABBIT_USER -e RABBITMQ_DEFAULT_PASS=$RABBIT_PASS radusimonica/test_distributor:$DOCKER_TAG java -jar /test-distributor/distributedTest.jar --publisher --suite $SUITE --rabbitHost 192.168.0.150 --requestQueue $QUEUE"
+                    }
+                }
+            }
+        }
+    }
+}
